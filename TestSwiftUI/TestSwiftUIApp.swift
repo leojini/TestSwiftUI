8ad6cec58b994a2ad3fe92b6678eaf04593360a4
leojini <leojini@gmail.com>
2024-01-27 20:42:51 +0900

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Leojin on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
